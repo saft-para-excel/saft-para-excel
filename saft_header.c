b6eb54e37f7111b94ee0bbe79e5b259429da8062
saft-para-excel <saftparaexcel@saftparaexcel.pt>
2024-11-17 13:48:46 +0000

    Erro na falta de AddressDetail
    Em alguns SAF-t da contabilidade do Primavera as tags AddressDetail e City não são fechadas, dando um erro grave.

--- saft_header.c
+++ saft_header.c
@@ -1,9 +1,68 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <psql.h>
 
 #define MAX_HEADER_DICT_SIZE 100
 #define MAX_HEADER_SIZE 256
+
+void insert_header(char *head_dict) {
+    const char *sql = "insert into header (audit_file_version, company_id, tax_registration_number, tax_accounting_basis, "
+                      "company_name, address_detail, city, postal_code, country, fiscal_year, "
+                      "start_date, end_date, currency_code, date_created, tax_entity, product_company_tax_id, "
+                      "software_certificate_number, product_id, product_version, telephone, email, "
+                      "website, file_hash, file_name) "
+                      "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    
+    char *data[24];
+    int index = 0;
+
+    // Simulating the dictionary access with a simple string parsing
+    char *audit_file_version = strstr(head_dict, "audit_file_version:") ? 
+                               strstr(head_dict, "audit_file_version:") + strlen("audit_file_version:") : 
+                               "1.04_01";
+    data[index++] = audit_file_version;
+
+    char *company_id = strstr(head_dict, "company_id:") ? 
+                       strstr(head_dict, "company_id:") + strlen("company_id:") : 
+                       NULL;
+    data[index++] = company_id;
+
+    char *tax_registration_number = strstr(head_dict, "tax_registration_number:") ? 
+                                     strstr(head_dict, "tax_registration_number:") + strlen("tax_registration_number:") : 
+                                     NULL;
+    data[index++] = tax_registration_number;
+
+    char *tax_accounting_basis = strstr(head_dict, "tax_accounting_basis:") ? 
+                                 strstr(head_dict, "tax_accounting_basis:") + strlen("tax_accounting_basis:") : 
+                                 NULL;
+    data[index++] = tax_accounting_basis;
+
+    char *company_name = strstr(head_dict, "company_name:") ? 
+                         strstr(head_dict, "company_name:") + strlen("company_name:") : 
+                         NULL;
+    data[index++] = company_name;
+
+    char *address_detail = strstr(head_dict, "address_detail:") ? 
+                           strstr(head_dict, "address_detail:") + strlen("address_detail:") : 
+                           "Sem Morada";
+    data[index++] = address_detail;
+
+    char *city = strstr(head_dict, "city:") ? 
+                 strstr(head_dict, "city:") + strlen("city:") : 
+                 "Sem Localidade";
+    data[index++] = city;
+
+    char *postal_code = strstr(head_dict, "postal_code:") ? 
+                        strstr(head_dict, "postal_code:") + strlen("postal_code:") : 
+                        "0000-000";
+    data[index++] = postal_code;
+
+    // Continue to fill the rest of the data array as needed...
+}
+
+
+
 
 
 typedef struct {
