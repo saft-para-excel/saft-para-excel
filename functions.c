39e0e4128f15b578093605f419264035ce947624
saft-para-excel <saftparaexcel@saftparaexcel.pt>
2024-12-07 17:47:48 +0000

    Update functions.c

--- functions.c
+++ functions.c
@@ -9,3 +9,23 @@ int remove_ns(const char *file_name) {
     return return_code;
 }
 
+
+int lint_file(const char *file_name) {
+    char backup_file[256];
+    snprintf(backup_file, sizeof(backup_file), "%s.bak", file_name);
+    
+    // Backup file
+    if (rename(file_name, backup_file) != 0) {
+        perror("Error renaming file");
+        return -1;
+    }
+
+    // Prepare command for linting
+    char command[512];
+    snprintf(command, sizeof(command), "xmllint %s --pretty 1 > %s", backup_file, file_name);
+
+    // Execute command
+    int return_code = system(command);
+    return return_code;
+}
+
